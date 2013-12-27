11c3937caae678d33953718083e86d624076956d
yeashiou33 <yeashiou33@gmail.com>
2013-12-27 16:50:13 +0800

    新增分頁功能

---
+++ source/src/Tabbed.java
@@ -0,0 +1,13 @@
+
+import javax.swing.*;
+public class Tabbed extends JTabbedPane{
+    ToolBar toolBar;
+    
+    Tabbed(){}
+
+    Tabbed(int TOP) {
+        super(TOP);
+        this.setBackground(null);
+    }
+    
+}
