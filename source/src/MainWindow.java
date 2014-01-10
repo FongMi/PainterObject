10514e62ab671c96433991c55c059159ce51c846
b8892778 <b8892778@gmail.com>
2014-01-10 10:24:34 +0800

    Undo MainWindow

--- source/src/MainWindow.java
+++ source/src/MainWindow.java
@@ -7,6 +7,7 @@ public class MainWindow extends JFrame {
     MenuBar menuBar;
     JTabbedPane tabbed;
     ToolBar toolBar;
+    ObjectToolBar objectToolBar;
     Page page;
     JLabel statusBar;
     
@@ -23,10 +24,19 @@ public class MainWindow extends JFrame {
         
         /*ToolBar*/
         toolBar = new ToolBar(this);
+        /*ObjectToolBar*/
+        objectToolBar = new ObjectToolBar(this);
         
+        /*設定 Tabbed 透明*/
+        UIManager.put("TabbedPane.contentOpaque", false);
+        /*Tabbed*/
+        tabbed = new JTabbedPane();
+        tabbed.add("小畫家", toolBar);
+        tabbed.add("物件", objectToolBar);
+
         /*MenuBar*/
         menuBar = new MenuBar(this);
-        menuBar.add(toolBar);
+        menuBar.add(tabbed);
         this.getContentPane().add(BorderLayout.NORTH, menuBar);
         
         /*Page*/
